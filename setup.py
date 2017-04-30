ce304509c97d1612082e259f166acd4f4633a2b4
aakash4525 <aksben65@gmail.com>
2017-04-30 17:02:41 +0530

    A new version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'link_preview', # name of package
     packages = ['link_preview'],
-    version = '0.0',
+    version = '0.1',
     description = 'Python package to get elements that make link preview',
     author = 'Akash Ahmed',
     author_email = 'aksben65@gmail.com',
     url = 'https://github.com/aakash4525/py_link_preview', # url of git repo
-    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.0.tar.gz', # git tagged tar.gz
+    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.1.tar.gz', # git tagged tar.gz
     keywords = ['preview', 'link'],
     classifiers = []
 )
