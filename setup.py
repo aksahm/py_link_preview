fb79b742e7d679e97875c42aead9b2af15863c07
aakash4525 <aksben65@gmail.com>
2017-05-02 03:56:24 +0530

    version

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ except:
 setup(
     name = 'link_preview', # name of package
     packages = ['link_preview'],
-    version = '0.4',
+    version = '0.5',
     description = 'Python package to get elements that make link preview',
     long_description=long_description,
     license='MIT',
     author = 'Akash Ahmed',
     author_email = 'aksben65@gmail.com',
     url = 'https://github.com/aakash4525/py_link_preview', # url of git repo
-    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.4.tar.gz', # git tagged tar.gz
+    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.5.tar.gz', # git tagged tar.gz
     keywords = ['preview', 'link'],
     platforms='all',
     classifiers = []
