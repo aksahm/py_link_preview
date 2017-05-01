da1378f56100a6b9db5d0cc2968fc2c55c20b4ba
aakash4525 <aksben65@gmail.com>
2017-05-02 03:01:53 +0530

    readme edited

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
-import os
+import os, markdown
 from distutils.core import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-    long_description = long_description.replace("\r","")
-except(IOError, ImportError):
-    print('Long desc failure')
-    long_description = open('README.md').read()
+long_description = markdown.html(os.path.join(os.path.abspath(__file__), 'README.md'))
 
 setup(
     name = 'link_preview', # name of package
     packages = ['link_preview'],
-    version = '0.6',
+    version = '0.7',
     description = 'Python package to get elements that make link preview',
     long_description=long_description,
     license='MIT',
     author = 'Akash Ahmed',
     author_email = 'aksben65@gmail.com',
     url = 'https://github.com/aakash4525/py_link_preview', # url of git repo
-    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.6.tar.gz', # git tagged tar.gz
+    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.7.tar.gz', # git tagged tar.gz
     keywords = ['preview', 'link'],
     platforms='all',
     classifiers = []
