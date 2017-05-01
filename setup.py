6c6c2e9082bc66f3bc580f59328d8a2443098c9e
aakash4525 <aksben65@gmail.com>
2017-05-02 02:45:29 +0530

    readme md to rst by pypandoc

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 import os
 from distutils.core import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
-    long_description = f.read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
     name = 'link_preview', # name of package
     packages = ['link_preview'],
-    version = '0.4',
+    version = '0.5',
     description = 'Python package to get elements that make link preview',
     long_description=long_description,
     license='MIT',
     author = 'Akash Ahmed',
     author_email = 'aksben65@gmail.com',
     url = 'https://github.com/aakash4525/py_link_preview', # url of git repo
-    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.4.tar.gz', # git tagged tar.gz
+    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.5.tar.gz', # git tagged tar.gz
     keywords = ['preview', 'link'],
     platforms='all',
     classifiers = []
