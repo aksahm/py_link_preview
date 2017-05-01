72c57cdbb31494247c06783c88d9c2f1385542d7
aakash4525 <aksben65@gmail.com>
2017-05-02 02:28:16 +0530

    readme edited

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 setup(
     name = 'link_preview', # name of package
     packages = ['link_preview'],
-    version = '0.3',
+    version = '0.4',
     description = 'Python package to get elements that make link preview',
     long_description=long_description,
     license='MIT',
     author = 'Akash Ahmed',
     author_email = 'aksben65@gmail.com',
     url = 'https://github.com/aakash4525/py_link_preview', # url of git repo
-    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.3.tar.gz', # git tagged tar.gz
+    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.4.tar.gz', # git tagged tar.gz
     keywords = ['preview', 'link'],
     platforms='all',
     classifiers = []
