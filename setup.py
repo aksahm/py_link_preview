6fd73f314242c2c03457c182f90a1365feb1b455
aakash4525 <aksben65@gmail.com>
2017-05-02 04:30:47 +0530

    setup edited

--- setup.py
+++ setup.py
@@ -7,19 +7,20 @@ try:
     long_description = long_description.replace("\r","")
 except:
     print('Long desc failure')
-    long_description = open(os.path.join(os.path.abspath(__file__),'README.md')).read()
+    with open(os.path.join(os.path.dirname(__file__),'README.md')) as f:
+        long_description = f.read()
 
 setup(
     name = 'link_preview', # name of package
     packages = ['link_preview'],
-    version = '0.6',
+    version = '0.7',
     description = 'Python package to get elements that make link preview',
     long_description=long_description,
     license='MIT',
     author = 'Akash Ahmed',
     author_email = 'aksben65@gmail.com',
     url = 'https://github.com/aakash4525/py_link_preview', # url of git repo
-    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.6.tar.gz', # git tagged tar.gz
+    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.7.tar.gz', # git tagged tar.gz
     keywords = ['preview', 'link'],
     platforms='all',
     classifiers = []
