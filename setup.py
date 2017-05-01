d338180e0bf82fa692c88bf6fb1b4958ced16324
aakash4525 <aksben65@gmail.com>
2017-05-02 04:58:38 +0530

    manifest added

--- setup.py
+++ setup.py
@@ -7,20 +7,19 @@ try:
     long_description = long_description.replace("\r","")
 except:
     print('Long desc failure')
-    with open(os.path.join(os.path.dirname(__file__),'README.md')) as f:
-        long_description = f.read()
+        long_description = open('README.md')read()
 
 setup(
     name = 'link_preview', # name of package
     packages = ['link_preview'],
-    version = '0.7',
+    version = '0.8',
     description = 'Python package to get elements that make link preview',
     long_description=long_description,
     license='MIT',
     author = 'Akash Ahmed',
     author_email = 'aksben65@gmail.com',
     url = 'https://github.com/aakash4525/py_link_preview', # url of git repo
-    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.7.tar.gz', # git tagged tar.gz
+    download_url = 'https://github.com/aakash4525/py_link_preview/archive/v0.8.tar.gz', # git tagged tar.gz
     keywords = ['preview', 'link'],
     platforms='all',
     classifiers = []
